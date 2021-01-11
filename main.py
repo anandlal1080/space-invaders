bc63df635c4de348031ea1d5a35483cffc6aa4a1
Allan V Nandlal <72092423+anandlal1080@users.noreply.github.com>
2021-01-10 22:05:53 -0700

    Added Spaceships

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -8,10 +9,25 @@ pygame.display.set_caption("First Game!")
 WHITE = (255, 255, 255)
 
 FPS = 60
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55, 40
+
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'spaceship_yellow.png'))
+
+YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+
+RED_SPACESHIP_IMAGE = pygame.image.load(
+    os.path.join('Assets', 'spaceship_red.png'))
+
+RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(
+    RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
 
 
 def draw_window():
     WIN.fill(WHITE)
+    WIN.blit(YELLOW_SPACESHIP, (300, 100))
+    WIN.blit(RED_SPACESHIP, (600, 100))
     pygame.display.update()
 
 
